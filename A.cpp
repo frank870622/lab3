fcb3253cc9dd5b2bfcfaca2745e38291bcdc9bde
frank870622 <frank870622@gmail.com>
2018-01-31 18:11:36 +0800

    2018/01/31 Hello World!!

--- A.cpp
+++ A.cpp
@@ -5,20 +5,19 @@
 using namespace std;
 int main(){
 	int n;
-	ifstream infile("file.in",ios::in);
+	ifstream infile("file.in", ios::in);
 	infile >> n;
 	vector <int> v1(n);
-	if ((n<5)||(n>10000)){
+	if ((n < 5) || (n > 10000)){
 		cout << "wrong number" << endl;
 		return 0;
 	}
-	for(int i=0;i<n;i++){
-		int temp = 0;
+	for(int i = 0; i < n; i++){
 		infile >> v1.at(i);
 	}
-	sort(v1.begin(),v1.end());
+	sort(v1.begin(), v1.end());
 	int ans = 0;
-	for(int i=n-1;i>n-6;--i) ans += v1.at(i);
+	for(int i = n - 1; i > n - 6; --i) ans += v1.at(i);
 	cout << ans << endl;
 	return 0;
 }
